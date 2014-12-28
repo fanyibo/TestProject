cfc7b653374c50fde01649d2d6390c0d96d072c5
fanyibo <yibo.f90@gmail.com>
2014-12-27 21:41:33 -0500

    12/27

--- src/com/fanyibo/util/Utils.java
+++ src/com/fanyibo/util/Utils.java
@@ -3064,27 +3064,6 @@ public class Utils {
         return result;
     }
 
-    /**
-     * Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from
-     * start
-     * to end, such that:
-     * Only one letter can be changed at a time
-     * Each intermediate word must exist in the dictionary
-     * For example,
-     * Given:
-     * start = "hit"
-     * end = "cog"
-     * dict = ["hot","dot","dog","lot","log"]
-     * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
-     * return its length 5.
-     * Note:
-     * Return 0 if there is no such transformation sequence.
-     * All words have the same length.
-     * All words contain only lowercase alphabetic characters.
-     */
-    public static int ladderLength(String start, String end, Set<String> dict) {
-        return 0;
-    }
 
     /**
      * Given a sorted linked list, delete all nodes that have duplicate numbers, leaving only distinct numbers from the
@@ -3092,9 +3071,48 @@ public class Utils {
      * For example,
      * Given 1->2->3->3->4->4->5, return 1->2->5.
      * Given 1->1->1->2->3, return 2->3.
+     * 1 1 2 3 4 4 5 6
      */
     public static ListNode deleteDuplicates1(ListNode head) {
-        return null;
+
+        if (head == null || head.next == null) {
+            return head;
+        }
+        HashMap<Integer, Integer> dupNum = new HashMap<Integer, Integer>();
+        ListNode temp = head;
+        while (temp != null) {
+
+            if (temp.next != null && temp.val == temp.next.val) {
+                dupNum.put(temp.val, temp.val);
+            }
+            temp = temp.next;
+        }
+
+        ListNode newHead = null;
+        ListNode tempHead = null;
+        temp = head;
+        while (temp != null) {
+
+            if (dupNum.containsKey(temp.val) == false) {
+                if (newHead == null) {
+                    newHead = temp;
+                    tempHead = newHead;
+                } else {
+                    tempHead.next = temp;
+                    tempHead = temp;
+                }
+                temp = temp.next;
+            } else {
+                if (tempHead != null) {
+                    temp = temp.next;
+                    tempHead.next = temp;
+                } else {
+                    temp = temp.next;
+                }
+            }
+        }
+
+        return newHead;
     }
 
     /**
@@ -3103,7 +3121,93 @@ public class Utils {
      * Would this affect the run-time complexity? How and why?
      * Write a function to determine if a given target is in the array.
      */
-    public static boolean search(int[] A, int target) {
+    public static int _search(int[] A, int target, int left, int right) {
+
+//        if (left > right || left < 0 || right >= A.length) {
+//            return -1;
+//        }
+//        if (right - left == 1) {
+//            return A[left] == target ? left : (A[right] == target ? right : -1);
+//        }
+//        if (left == right) {
+//            return A[left] == target ? left : -1;
+//        }
+//        int half = ((right - left) / 2) + left;
+//
+//        if (A[left] < A[half]) {
+//
+//            if (A[left] <= target && A[half] >= target) {
+//                return _search(A, target, left, half);
+//            } else {
+//                return _search(A, target, half + 1, right);
+//            }
+//
+//        } else {
+//
+//            if (A[half] <= target && A[right] >= target) {
+//                return _search(A, target, half, right);
+//            } else {
+//                return _search(A, target, left, half - 1);
+//            }
+//        }
+
+        if (left > right || left < 0 || right >= A.length) {
+            return -1;
+        }
+        if (right - left == 1) {
+            return A[left] == target ? left : (A[right] == target ? right : -1);
+        }
+        if (left == right) {
+            return A[left] == target ? left : -1;
+        }
+
+        int half = ((right - left) / 2) + left;
+        if (A[left] < A[half] && A[left] <= target && A[half] >= target) {
+            return _search(A, target, left, half);
+        } else {
+            for (int i = half + 1; i < right; i++) {
+                if (A[i] == target) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public static int search1(int[] A, int target) {
+
+        if (A == null || A.length == 0) {
+            return -1;
+        }
+
+        return _search(A, target, 0, A.length - 1);
+    }
+
+    public static boolean search2(int[] A, int target) {
+
+        int start = 0;
+        int end = A.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (A[mid] == target) {
+                return true;
+            }
+            if (A[start] < A[mid]) {
+                if (target >= A[start] && target < A[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } else if (A[start] > A[mid]) {
+                if (target > A[mid] && target <= A[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                start++;
+            }
+        }
         return false;
     }
 
@@ -3115,133 +3219,53 @@ public class Utils {
      * Your function should return length = 5, and A is now [1,1,2,2,3].
      */
     public static int removeDuplicates2(int[] A) {
-        return 0;
-    }
 
-    /**
-     * Given a set of distinct integers, S, return all possible subsets.
-     * Note:
-     * Elements in a subset must be in non-descending order.
-     * The solution set must not contain duplicate subsets.
-     * For example,
-     * If S = [1,2,3], a solution is:
-     * [
-     * [3],
-     * [1],
-     * [2],
-     * [1,2,3],
-     * [1,3],
-     * [2,3],
-     * [1,2],
-     * []
-     * ]
-     */
-    public static List<List<Integer>> subsets(int[] S) {
-        return null;
-    }
-
-    /**
-     * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
-     * For example,
-     * If n = 4 and k = 2, a solution is:
-     * [
-     * [2,4],
-     * [3,4],
-     * [2,3],
-     * [1,2],
-     * [1,3],
-     * [1,4],
-     * ]
-     */
-    public static List<List<Integer>> combine(int n, int k) {
-        return null;
-    }
-
-    /**
-     * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
-     * click to show follow up.
-     * Follow up:
-     * Did you use extra space?
-     * A straight forward solution using O(mn) space is probably a bad idea.
-     * A simple improvement uses O(m + n) space, but still not the best solution.
-     * Could you devise a constant space solution?
-     */
-    public static void setZeroes(int[][] matrix) {
-
-    }
-
-    /**
-     * Given an absolute path for a file (Unix-style), simplify it.
-     * For example,
-     * path = "/home/", => "/home"
-     * path = "/a/./b/../../c/", => "/c"
-     * click to show corner cases.
-     * Corner Cases:
-     * Did you consider the case where path = "/../"?
-     * In this case, you should return "/".
-     * Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
-     * In this case, you should ignore redundant slashes and return "/home/foo".
-     */
-    public static String simplifyPath(String path) {
-        return null;
-    }
-
-    /**
-     * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
-     * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right
-     * corner of the grid (marked 'Finish' in the diagram below).
-     * How many possible unique paths are there?
-     * Above is a 3 x 7 grid. How many possible unique paths are there?
-     * Note: m and n will be at most 100.
-     */
-    public static int uniquePaths(int m, int n) {
-        return 0;
-    }
-
-    /**
-     * Given a collection of intervals, merge all overlapping intervals.
-     * For example,
-     * Given [1,3],[2,6],[8,10],[15,18],
-     * return [1,6],[8,10],[15,18].
-     */
-    public static class Interval {
-        int start;
-        int end;
-
-        Interval() {
-            start = 0;
-            end = 0;
+        if (A == null || A.length <= 2) {
+            return A.length;
         }
 
-        Interval(int s, int e) {
-            start = s;
-            end = e;
+        HashMap<Integer, Integer> dupNum = new HashMap<Integer, Integer>();
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < A.length - 1; i++) {
+            if (A[i] == A[i + 1]) {
+                if (dupNum.containsKey(A[i])) {
+                    int count = dupNum.get(A[i]);
+                    if (count < 2) {
+                        dupNum.put(A[i], count + 1);
+                    }
+                } else {
+                    dupNum.put(A[i], 2);
+                    list.add(A[i]);
+                }
+            } else {
+                if (!dupNum.containsKey(A[i])) {
+                    dupNum.put(A[i], 1);
+                    list.add(A[i]);
+                }
+                if (i == A.length - 2) {
+                    dupNum.put(A[i + 1], 1);
+                    list.add(A[i + 1]);
+                }
+            }
         }
+        int index = 0;
+        for (Integer key : list) {
+            int count = dupNum.get(key);
+            for (int i = 0; i < count; i++) {
+                A[index + i] = key;
+            }
+            index += count;
+        }
+        return index;
     }
 
-    public static List<Interval> merge(List<Interval> intervals) {
-        return null;
-    }
-
-    /**
-     * Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
-     * For example,
-     * Given n = 3, your program should return all 5 unique BST's shown below.
-     * 1         3     3      2      1
-     * \       /     /      / \      \
-     * 3     2     1      1   3      2
-     * /     /       \                 \
-     * 2     1         2                 3
-     */
-    public static List<TreeNode> generateTrees(int n) {
-        return null;
-    }
 
     public static void main(String[] args) {
 
-        List<Integer> list = grayCode(2);
-        System.out.println(list.toString());
 
+        int[] A = {1, 3, 1, 1, 1, 1};
+
+        System.out.println(search2(A, 1));
 
 //        TreeNode n1 = new TreeNode(10);
 //        TreeNode n2 = new TreeNode(5);
---
+++ src/com/fanyibo/util/Utils2.java
@@ -0,0 +1,206 @@
+/*
+ *    Copyright   2014
+ *     Filename : Utils2
+ *      Project : TestProject
+ *   Created by : fanyibo on 12/27/14 9:37 PM
+ */
+package com.fanyibo.util;
+
+import com.fanyibo.tree.TreeNode;
+
+import java.util.List;
+import java.util.Set;
+
+public final class Utils2 {
+    /**
+     * Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique
+     * triplets
+     * in the array which gives the sum of zero.
+     * Note:
+     * Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c)
+     * The solution set must not contain duplicate triplets.
+     * For example, given array S = {-1 0 1 2 -1 -4},
+     * A solution set is:
+     * (-1, 0, 1)
+     * (-1, -1, 2)
+     */
+    public static List<List<Integer>> threeSum(int[] num) {
+        return null;
+    }
+
+    /**
+     * Given an array S of n integers, find three integers in S such that the sum is closest to a given number, target.
+     * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+     * For example, given array S = {-1 2 1 -4}, and target = 1.
+     * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+     */
+    public static int threeSumClosest(int[] num, int target) {
+        return 0;
+    }
+
+
+    /**
+     * Given an array S of n integers, are there elements a, b, c, and d in S such that a + b + c + d = target? Find
+     * all
+     * unique quadruplets in the array which gives the sum of target.
+     * Note:
+     * Elements in a quadruplet (a,b,c,d) must be in non-descending order. (ie, a ≤ b ≤ c ≤ d)
+     * The solution set must not contain duplicate quadruplets.
+     * For example, given array S = {1 0 -1 0 -2 2}, and target = 0.
+     * A solution set is:
+     * (-1,  0, 0, 1)
+     * (-2, -1, 1, 2)
+     * (-2,  0, 0, 2)
+     */
+    public static List<List<Integer>> fourSum(int[] num, int target) {
+        return null;
+    }
+
+
+    /**
+     * Given two words (start and end), and a dictionary, find the length of shortest transformation sequence from
+     * start
+     * to end, such that:
+     * Only one letter can be changed at a time
+     * Each intermediate word must exist in the dictionary
+     * For example,
+     * Given:
+     * start = "hit"
+     * end = "cog"
+     * dict = ["hot","dot","dog","lot","log"]
+     * As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
+     * return its length 5.
+     * Note:
+     * Return 0 if there is no such transformation sequence.
+     * All words have the same length.
+     * All words contain only lowercase alphabetic characters.
+     */
+    public static int ladderLength(String start, String end, Set<String> dict) {
+        return 0;
+    }
+
+    /**
+     * Given a set of distinct integers, S, return all possible subsets.
+     * Note:
+     * Elements in a subset must be in non-descending order.
+     * The solution set must not contain duplicate subsets.
+     * For example,
+     * If S = [1,2,3], a solution is:
+     * [
+     * [3],
+     * [1],
+     * [2],
+     * [1,2,3],
+     * [1,3],
+     * [2,3],
+     * [1,2],
+     * []
+     * ]
+     */
+    public static List<List<Integer>> subsets(int[] S) {
+        return null;
+    }
+
+    /**
+     * Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+     * For example,
+     * If n = 4 and k = 2, a solution is:
+     * [
+     * [2,4],
+     * [3,4],
+     * [2,3],
+     * [1,2],
+     * [1,3],
+     * [1,4],
+     * ]
+     */
+    public static List<List<Integer>> combine(int n, int k) {
+        return null;
+    }
+
+    /**
+     * Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+     * click to show follow up.
+     * Follow up:
+     * Did you use extra space?
+     * A straight forward solution using O(mn) space is probably a bad idea.
+     * A simple improvement uses O(m + n) space, but still not the best solution.
+     * Could you devise a constant space solution?
+     */
+    public static void setZeroes(int[][] matrix) {
+
+    }
+
+    /**
+     * Given an absolute path for a file (Unix-style), simplify it.
+     * For example,
+     * path = "/home/", => "/home"
+     * path = "/a/./b/../../c/", => "/c"
+     * click to show corner cases.
+     * Corner Cases:
+     * Did you consider the case where path = "/../"?
+     * In this case, you should return "/".
+     * Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+     * In this case, you should ignore redundant slashes and return "/home/foo".
+     */
+    public static String simplifyPath(String path) {
+        return null;
+    }
+
+    /**
+     * A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+     * The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right
+     * corner of the grid (marked 'Finish' in the diagram below).
+     * How many possible unique paths are there?
+     * Above is a 3 x 7 grid. How many possible unique paths are there?
+     * Note: m and n will be at most 100.
+     */
+    public static int uniquePaths(int m, int n) {
+        return 0;
+    }
+
+    /**
+     * Given a collection of intervals, merge all overlapping intervals.
+     * For example,
+     * Given [1,3],[2,6],[8,10],[15,18],
+     * return [1,6],[8,10],[15,18].
+     */
+    public static class Interval {
+        int start;
+        int end;
+
+        Interval() {
+            start = 0;
+            end = 0;
+        }
+
+        Interval(int s, int e) {
+            start = s;
+            end = e;
+        }
+
+    }
+
+    public static List<Interval> merge(List<Interval> intervals) {
+        return null;
+    }
+
+    /**
+     * Given n, generate all structurally unique BST's (binary search trees) that store values 1...n.
+     * For example,
+     * Given n = 3, your program should return all 5 unique BST's shown below.
+     * 1         3     3      2      1
+     * \       /     /      / \      \
+     * 3     2     1      1   3      2
+     * /     /       \                 \
+     * 2     1         2                 3
+     */
+    public static List<TreeNode> generateTrees(int n) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
