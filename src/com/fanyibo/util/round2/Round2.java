1c72f637f8946b22987639673c06ed8157b1a127
fanyibo <yibo.f90@gmail.com>
2015-01-29 18:36:58 -0500

    01/29

--- src/com/fanyibo/util/round2/Round2.java
+++ src/com/fanyibo/util/round2/Round2.java
@@ -7966,7 +7966,68 @@ public class Round2 {
     }
 
 
+    public static long maxHeight(int N, int K, int[] x, int[] t) {
+
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < x.length; i++) {
+            map.put(x[i], t[i]);
+        }
+
+        long[] height = new long[N];
+        long[] max = new long[N];
+        for (int i = 1; i < N; i++) {
+            int number = i + 1;
+            if (map.containsKey(number)) {
+                int maxH = map.get(number);
+                long h = Math.min(maxH, height[i - 1] + K);
+                if (Math.abs(h - height[i - 1]) > K) {
+                    long diff = height[i - 1] - h;
+                    for (int j = i - 1; j >= 1; j--) {
+                        if (height[j] - diff < 0) {
+                            height[j] = 0;
+                        } else {
+                            height[j] -= diff;
+                        }
+                    }
+                    max[0] = 0;
+                    for (int j = 1; j <= i - 1; j++) {
+                        max[j] = Math.max(max[j - 1], height[j]);
+                    }
+                }
+                height[i] = h;
+                max[i] = Math.max(max[i - 1], height[i]);
+            } else {
+                long h = height[i - 1] + K;
+                height[i] = h;
+                max[i] = Math.max(max[i - 1], height[i]);
+            }
+        }
+        return max[N - 1];
+    }
+
+
     public static void main(String[] args) {
+
+        PRINT("3 => " + maxHeight(10, 1, new int[]{3, 8}, new int[]{1, 1}));
+        PRINT("999999999000000000 => " + maxHeight(1000000000, 1000000000, new int[]{}, new int[]{}));
+        PRINT("22 => " + maxHeight(20, 3,
+                                   new int[]{34, 35, 48, 86, 110, 170, 275, 288, 313, 321, 344, 373, 390, 410, 412, 441,
+                                             499, 525, 538, 568, 585, 627, 630, 671, 692, 699, 719, 752, 755, 764, 772},
+                                   new int[]{89, 81, 88, 42, 55, 92, 19, 91, 71, 42, 72, 18, 86, 89, 88, 75, 29, 98, 63,
+                                             74, 45, 11, 68, 34, 94, 20, 69, 33, 50, 69, 54}));
+        PRINT("28495511604 => " + maxHeight(780, 547990606, new int[]{3, 8}, new int[]{1, 1}));
+        PRINT("1365130725 => " + maxHeight(7824078, 2374,
+                                           new int[]{134668, 488112, 558756, 590300, 787884, 868112, 1550116, 1681439,
+                                                     1790994, 1796091, 1906637, 2005485, 2152813, 2171716, 2255697,
+                                                     2332732, 2516853, 2749024, 2922558, 2930163, 3568190, 3882735,
+                                                     4264888, 5080550, 5167938, 5249191, 5574341, 5866912, 5936121,
+                                                     6142348, 6164177, 6176113, 6434368, 6552905, 6588059, 6628843,
+                                                     6744163, 6760794, 6982172, 7080464, 7175493, 7249044},
+                                           new int[]{8, 9, 171315129, 52304509, 1090062, 476157338, 245, 6, 253638067,
+                                                     37, 500, 29060, 106246500, 129, 22402, 939993108, 7375, 2365707,
+                                                     40098, 10200444, 3193547, 55597, 24920935, 905027, 1374, 12396141,
+                                                     525886, 41, 33, 3692, 11502, 180, 3186, 5560, 778988, 42449532,
+                                                     269666, 10982579, 48, 3994, 1, 9}));
 
     }
 
